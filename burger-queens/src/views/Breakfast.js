3579b3c189bf049cd7ee6f8166b25b0e6db58a4e
dacoquima <danimalistica@gmail.com>
2019-09-25 18:10:48 -0300

    Se agrega view Breakfast

--- burger-queens/src/views/Breakfast.js
+++ burger-queens/src/views/Breakfast.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import data from '../data/data.json';
+import menu from '../data/data.json';
+import './Breakfast.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function Breakfast() {
     return (
-        <container>
+    <container>
         <Navigation home="INICIO" menu="MENU" pending="PENDIENTES" records="HISTORIAL"/>
         <Link to="/breakfast"> <button type="button">DESAYUNO</button></Link>
         <Link to="/lunch"><button type="button">ALMUERZO Y CENA</button></Link>
 
-        {data.Breakfasts.map((e =>
-        <button product={e.product} price={e.price} ></button>
+        <br></br>
+    <div className="containerBreakfast">
+        <div className="breakfastProducts">
+        {menu.Breakfasts.map((e =>
+            <button className="btnBreakfast"> 
+                <div> {e.img} </div>
+                <div> {e.product} </div>
+                <div> {e.price} </div>
+            </button>
         ))}
-        </container>
-        )
+        </div>
+
+        <div className="itemMenu">
+            <h1>DETALLE DE LA ORDEN</h1>
+        </div>
+    </div>
+    </container>
+    )
 }
 
 export default Breakfast;
