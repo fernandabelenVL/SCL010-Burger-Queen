ebe4bf46c9183d90bbaf7199e52316fd396b3c21
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-09-25 18:11:55 -0300

    tarjetas de menu items en lunch

--- burger-queens/src/views/Lunch.js
+++ burger-queens/src/views/Lunch.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import './Lunch.css';
+
+
+import { Link } from "react-router-dom";
+
+// data
+import menu from '../data/data.json';
+
 
 function Lunch() {
     return (
-        <Navigation  title="Home"/>
-        )
+        <div>
+            <Navigation home="INICIO" menu="MENU" pending="PENDIENTES" records="HISTORIAL"/>
+
+            <Link to="/breakfast"> <button type="button"> DESAYUNO </button></Link>
+		    <Link to="/lunch"><button type="button"> ALMUERZO </button></Link>
+
+            <h1>ALMUERZO</h1>
+
+            
+            <div className="lunch-content">
+                <div className ="lunch-menu">
+                    {menu.Lunchs.map((e =>
+                    <button className="menu-item">
+                        <img src={e.img} alt="ico"/>
+                        <div>{e.product}</div>
+                        <div>{e.price}</div>
+                    </button>    
+                    ))}
+                </div>
+                <div className="order-detail">
+                    <h1>Detalle de la orden</h1>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Lunch;
