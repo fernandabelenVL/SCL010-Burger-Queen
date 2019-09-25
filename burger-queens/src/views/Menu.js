a1fd61d4632a1f6854b17537a8bdd8272c0c9cf4
olgamperez <ing.olgaperez@gmail.com>
2019-09-25 18:28:42 -0300

    en proceso de input y estilo de este

--- burger-queens/src/views/Menu.js
+++ burger-queens/src/views/Menu.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
 
 function Menu() {
     return (
         <div>
+            <div>
             <Navigation home="INICIO" menu="MENU" pending="PENDIENTES" records="HISTORIAL"/>
             <h1>MENU</h1>
-            <Link to="/breakfast"> <button type="button"> DESAYUNO </button></Link>
-		    <Link to="/lunch"><button type="button"> ALMUERZO </button></Link>
+            </div>
+        
+            <div className="button-menu">
+            <button className="">DESAYUNO</button>
+            <button>ALMUERZO Y CENA</button>
+            </div>
+            <div>
+                <p>NOMBRE DEL CLIENTE</p>
+                <input></input>
+            </div>
         </div>
     )
 }
