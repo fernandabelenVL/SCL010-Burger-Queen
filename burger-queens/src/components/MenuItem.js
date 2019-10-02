651a22360dc647b1ed76765e7391bcf03c9f5d02
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-10-02 14:43:43 -0300

    leyendo nombre y precio de elementos de menu

--- burger-queens/src/components/MenuItem.js
+++ burger-queens/src/components/MenuItem.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MenuItem.css';
 
 
-function MenuItem (props) {
+class MenuItem extends Component {
+    constructor (){
+        super();
+        this.state = {
+            product: "",
+            price: ""
+        }
+        this.changeNameAndPrice= this.changeNameAndPrice.bind(this)
+    }
+
+    changeNameAndPrice (props){
+        this.setState({product: this.props.product, price: this.props.price})
+        console.log(this.state.product)
+    }
+
+    render(){
     return (
-            <button className = 'item-box'>
-                <img src={props.img} alt="menu icon" className="item-img"/>
-                <div className='item-name'><p>{props.product}</p></div>
-                <div className='item-price'><p>{props.price}</p></div>
+            <button className = 'item-box' onClick= {this.changeNameAndPrice(this.props)}>
+                <img src={this.props.img} alt="menu icon" className="item-img"/>
+                <div className='item-name'><p>{this.props.product}</p></div>
+                <div className='item-price'><p>{this.props.price}</p></div>
             </button>
     )   
+    }
 }
 
 export default MenuItem;
