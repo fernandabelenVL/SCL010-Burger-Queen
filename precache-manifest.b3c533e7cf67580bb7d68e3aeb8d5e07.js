2c6c108509417112f03a7b502b0046fe38ca4bd7
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-10-04 17:17:14 -0300

    Updates

---
+++ precache-manifest.b3c533e7cf67580bb7d68e3aeb8d5e07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb696a71626b429c47a7258a38a0f46",
+    "url": "/SCL010-Burger-Queen/index.html"
+  },
+  {
+    "revision": "7d32747bcf1e63d149bb",
+    "url": "/SCL010-Burger-Queen/static/css/2.cb0a7b97.chunk.css"
+  },
+  {
+    "revision": "7665b212fb324cc137bb",
+    "url": "/SCL010-Burger-Queen/static/css/main.2c675e6d.chunk.css"
+  },
+  {
+    "revision": "7d32747bcf1e63d149bb",
+    "url": "/SCL010-Burger-Queen/static/js/2.37cc7e9b.chunk.js"
+  },
+  {
+    "revision": "7665b212fb324cc137bb",
+    "url": "/SCL010-Burger-Queen/static/js/main.1ed21571.chunk.js"
+  },
+  {
+    "revision": "3a821617a4d4dba1cad2",
+    "url": "/SCL010-Burger-Queen/static/js/runtime~main.35e6da41.js"
+  },
+  {
+    "revision": "8a300f700382461962dc91e01a63c8cf",
+    "url": "/SCL010-Burger-Queen/static/media/bg-image@2x.8a300f70.png"
+  },
+  {
+    "revision": "5d9005735e8efd7059e9e949947b351d",
+    "url": "/SCL010-Burger-Queen/static/media/big-burger@2x.5d900573.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SCL010-Burger-Queen/precache-manifest.b3c533e7cf67580bb7d68e3aeb8d5e07.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SCL010-Burger-Queen/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
